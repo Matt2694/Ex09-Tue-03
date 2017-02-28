dfe48098300f40fe3f9120bad971b4358e15310b
Matthew Anthony Peterson <matt2694@edu.eal.dk>
2017-02-28 09:32:58 +0100

    Add project files.

---
+++ Ex09-Tue-03/View Models/OrangeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex09_Tue_03.View_Models
+{
+    class OrangeViewModel
+    {
+    }
+}
